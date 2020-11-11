5a57b80252b489236a061993d61202346acdb630
Zijian Zhang <zzhang@l3s.de>
2020-11-11 10:28:31 +0100

    update script from n2

---
+++ scripts/sweep_fever_0.02_0.09.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+GPU_NUM=$1
+
+RUN_CLEAN=$2
+
+pushd ../
+
+mkdir -p logs
+
+if [ clean == "$RUN_CLEAN" ];
+then
+	rm ./logs/sweep_fever_0.0*
+fi
+
+for w_exp in 0.0{2..9};
+do
+	date >> logs/sweep_fever_0.02_0.09.stdout
+	date >> logs/sweep_fever_0.02_0.09.stderr
+	CUDA_VISIBLE_DEVICES=$GPU_NUM PYTHONPATH=$PYTHONPATH:./ python latent_rationale/e2expred/train.py --conf_fname ./parameters/fever_e2expred.json --dataset_name fever --w_aux 1.0 --w_exp $w_exp --data_dir ../data --save_path ./results/e2expred/separate_encoder_cold_start --batch_size 13 --train_on_part 0.1 1>>logs/sweep_fever_0.02_0.09.stdout 2>>logs/sweep_fever_0.02_0.09.stderr
+	date >> logs/sweep_fever_0.02_0.09.stdout
+	date >> logs/sweep_fever_0.02_0.09.stderr
+done
+
+popd
---
+++ scripts/test.py
@@ -0,0 +1,3 @@
+import torch
+import transformers
+print(torch.LongTensor([1,2,3,4,5]).to('cuda'))
