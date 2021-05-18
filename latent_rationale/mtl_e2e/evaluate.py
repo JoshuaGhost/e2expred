use wandb to do the record, fix inadequate attention mask

latent_rationale/mtl_e2e/evaluate.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report, accuracy_score
 
 from latent_rationale.common.util import get_minibatch
 from latent_rationale.mtl_e2e.metrics import cls_prf
-from latent_rationale.mtl_e2e.utils import prepare_minibatch
+from latent_rationale.mtl_e2e.utils import bert_input_preprocess
 from latent_rationale.common.util import get_z_stats
 
 
@@ -59,11 +59,17 @@ def evaluate(model, data, tokenizer,
     masks_total = []
     labels_total = []
     exp_labels_total = []
-    for batch in get_minibatch(data, batch_size=batch_size, shuffle=False):
-        inputs, exp_labels, labels, positions, attention_masks, padding_masks = prepare_minibatch(batch,
-                                                                                                  tokenizer=tokenizer,
-                                                                                                  max_length=max_length,
-                                                                                                  device=device)
+    for batch in data:
+        inputs, exp_labels, cls_labels, positions, attention_masks, padding_masks = batch
+
+        exp_labels = exp_labels.cuda()
+        cls_labels = cls_labels.cuda()
+        padding_masks = padding_masks.cuda()
+
+        # inputs, exp_labels, cls_labels, positions, attention_masks, padding_masks = bert_input_preprocess(batch,
+        #                                                                                           tokenizer=tokenizer,
+        #                                                                                           max_length=max_length,
+        #                                                                                           device=device)
         batch_size = len(batch)
         # print(inputs.data.shape)
         with torch.no_grad():
@@ -76,9 +82,9 @@ def evaluate(model, data, tokenizer,
             soft_exp_pred_total.extend(soft_exp_pred)
             hard_exp_pred_total.extend(hard_exp_pred)
             masks_total.extend(attention_masks)
-            labels_total.extend(labels)
+            labels_total.extend(cls_labels)
             exp_labels_total.extend(exp_labels.data)
-            loss, loss_optional = model.get_loss(aux_pred_p, cls_pred_p, labels, hard_exp_pred, exp_labels.data,
+            loss, loss_optional = model.get_loss(aux_pred_p, cls_pred_p, cls_labels, hard_exp_pred, exp_labels.data,
                                                  padding_masks, weights)
 
             results['loss'] += loss.item() * batch_size

latent_rationale/mtl_e2e/models/components.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 from transformers.file_utils import add_start_docstrings_to_callable
 from transformers.modeling_bert import BERT_INPUTS_DOCSTRING
 
+from latent_rationale.mtl_e2e.config import MTLConfig, SelectorConfig
 from latent_rationale.mtl_e2e.utils import PaddedSequence
 from latent_rationale.nn.kuma_gate import KumaGate
 from latent_rationale.common.latent import IndependentSelector
@@ -93,11 +94,11 @@ class BertDataPreprocessor(nn.Module):
 
 class KumaSelectorLayer(IndependentSelector):
     def __init__(self,
-                 selector_params,
+                 selector_params: SelectorConfig,
                  repr_size: int):
         super(KumaSelectorLayer, self).__init__()
-        dropout = selector_params['dropout']
-        distribution = selector_params['dist']
+        dropout = selector_params.dropout
+        distribution = selector_params.dist
         self.selector_head = nn.Sequential(
             nn.Dropout(p=dropout),
             nn.Linear(in_features=repr_size, out_features=repr_size),
@@ -113,7 +114,7 @@ class KumaSelectorLayer(IndependentSelector):
         self.z_dists = []  # z distribution(s)
         self.report_params()
 
-    def forward(self, x, mask, **kwargs):
+    def forward(self, x, mask, **kwargs) -> torch.Tensor:
 
         # encode sentence
         # lengths = mask.sum(1)
@@ -270,18 +271,18 @@ class BertModelWithEmbeddingMask(BertModel):
 
 class BertClassifier(nn.Module):
     def __init__(self,
-                 classifier_params,
+                 classifier_params: MTLConfig,
                  tokenizer: BertTokenizer, ):
         super(BertClassifier, self).__init__()
-        self.num_labels = classifier_params['num_labels']
-        self.cls_params = classifier_params['cls_head']
-        self.max_length = classifier_params['max_length']
+        self.num_labels = classifier_params.num_labels
+        self.cls_params = classifier_params.cls_head
+        self.max_length = classifier_params.max_length
         self.pad_token_id = tokenizer.pad_token_id
         self.cls_token_id = tokenizer.cls_token_id
         self.sep_token_id = tokenizer.sep_token_id
-        bert_dir = classifier_params['bert_dir']
+        bert_dir = classifier_params.bert_dir
         bert_model = BertModelWithEmbeddingMask.from_pretrained(bert_dir)
-        if bool(classifier_params['use_half_precision']):
+        if bool(classifier_params.use_half_precision):
             import apex
             bert_model = bert_model.half()
         self.bert_model = bert_model

latent_rationale/mtl_e2e/models/expred_e2e.py
@@ -1,58 +1,61 @@
-from typing import Any
+from typing import Tuple, Dict
 
 from sklearn.metrics import accuracy_score
 from transformers import BertTokenizer
 import torch
 from torch import nn
 from latent_rationale.common.util import get_z_stats
+from latent_rationale.mtl_e2e.config import E2ExPredConfig
 
 from latent_rationale.mtl_e2e.models.components import BertClassifier, KumaSelectorLayer
 
 
 class HardKumaE2E(nn.Module):
     def __init__(self,
-                 cfg: Any,
+                 cfg: E2ExPredConfig,
                  tokenizer: BertTokenizer,
                  epochs_total: int):
         super(HardKumaE2E, self).__init__()
 
         self.epochs_total = epochs_total
-        self.weights_scheduler = cfg['weights_scheduler']
-        self.lambda_min = cfg['lambda_min']
-        self.lambda_max = cfg['lambda_max']
-        mtl_params = cfg['mtl']
+        self.weights_scheduler = cfg.weights_scheduler
+        self.lambda_min = cfg.lambda_min
+        self.lambda_max = cfg.lambda_max
+        mtl_params = cfg.mtl_conf
         self.bert_mtl = BertClassifier(mtl_params, tokenizer)
         # self.bert_mtl_exp = nn.Linear(300, 768)
         self.auxilliary_criterion = nn.CrossEntropyLoss(reduction='none')
 
         bert_ebd_dims = self.bert_mtl.bert_model.config.hidden_size
-        selector_params = cfg['selector']
-        if selector_params['selector_type'] == 'hard binary':
+        selector_params = cfg.selector_conf
+        if selector_params.selector_type == 'hard binary':
             self.rationale_selector = KumaSelectorLayer(selector_params,
                                                         repr_size=bert_ebd_dims)
         else:
             raise NotImplementedError
         from latent_rationale.common.losses import resampling_rebalanced_crossentropy
         self.exp_criterion = resampling_rebalanced_crossentropy(seq_reduction='mean')
-        self.exp_threshold = selector_params.get('exp_threshold', 0.5)
+        self.exp_threshold = selector_params.exp_threshold
 
-        classifier_params = cfg['cls']
-        self.shared_encoder = cfg.get('share_encoder', False)
-        if self.shared_encoder:
+        classifier_params = cfg.cls_conf
+        self.share_encoder = cfg.share_encoder
+        if self.share_encoder:
             self.classifier = self.bert_mtl
         else:
             self.classifier = BertClassifier(classifier_params, tokenizer)
         self.final_cls_criterion = nn.CrossEntropyLoss(reduction='none')
 
-        self.soft_selection = cfg['soft_selection']
+        self.soft_selection = selector_params.soft_selection
 
-        if cfg.get('warm_start_mtl', None) is not None:
-            self.bert_mtl.bert_model.load_state_dict(torch.load(cfg['warm_start_mtl']))
-        if cfg.get('warm_start_cls', None) is not None and not self.shared_encoder:
+        if cfg.mtl_conf.warm_start:
+            state_dict = torch.load(cfg.mtl_conf.pretrained_model_dir)
+            self.bert_mtl.bert_model.load_state_dict(state_dict)
+        if cfg.cls_conf.warm_start and not self.share_encoder:
             # If the encoder is shared, the cls warm start is muted
-            self.classifier.bert_model.load_state_dict(torch.load(cfg['warm_start_cls']))
+            state_dict = torch.load(cfg.cls_conf.pretrained_model_dir)
+            self.classifier.bert_model.load_state_dict(state_dict)
 
-        lambda_init = cfg['lambda_init']
+        lambda_init = cfg.lambda_init
         self.register_buffer('lambda0', torch.full((1,), lambda_init))
         self.register_buffer('lambda1', torch.full((1,), lambda_init))
         self.register_buffer('c0_ma', torch.full((1,), 0.))  # moving average
@@ -63,34 +66,43 @@ class HardKumaE2E(nn.Module):
         return cls(cfg, tokenizer, epochs_total)
 
     def forward(self,
-                bert_inputs: torch.Tensor,
+                inputs: torch.Tensor,
                 attention_masks: torch.Tensor,
                 padding_masks: torch.Tensor,
                 positions: torch.Tensor):
         device = next(self.parameters()).device
 
-        auxiliary_cls_p, mtl_exp_output_hidden = self.bert_mtl(bert_inputs,
+        inputs = inputs.to(device)
+        attention_masks = attention_masks.to(device)
+        padding_masks = padding_masks.to(device)
+        positions = positions.to(device)
+
+        auxiliary_cls_p, mtl_exp_output_hidden = self.bert_mtl(inputs,
                                                                attention_masks=attention_masks,
                                                                positions=positions)
         soft_z = self.rationale_selector(mtl_exp_output_hidden, padding_masks).squeeze(dim=-1)
+        hard_z = torch.where(soft_z >= self.exp_threshold,
+                             torch.ones_like(soft_z).to(device=device),
+                             torch.zeros_like(soft_z).to(device=device))
         if self.training:
-            hard_z = torch.where(soft_z >= self.exp_threshold, torch.LongTensor([1]).to(device=device),
-                                 torch.LongTensor([0]).to(device=device))
+
             hard_z = (hard_z - soft_z).detach() + soft_z
-        else:
-            hard_z = torch.where(soft_z >= self.exp_threshold, torch.LongTensor([1]).to(device=device),
-                                 torch.LongTensor([0]).to(device=device))
+        # else:
+        #     hard_z = torch.where(soft_z >= self.exp_threshold, torch.LongTensor([1]).to(device=device),
+        #                          torch.LongTensor([0]).to(device=device))
         if self.soft_selection:
             attribution_map = soft_z
         else:
             attribution_map = hard_z
-        cls_p, _ = self.classifier(bert_inputs,
+        cls_p, _ = self.classifier(inputs,
                                    attention_masks=attention_masks,
                                    embedding_masks=attribution_map,
                                    positions=positions)
         return auxiliary_cls_p, cls_p, soft_z, hard_z
 
-    def get_loss(self, p_aux, p_cls, t_cls, p_exp, t_exp, mask, weights, epoch=None):
+    def get_loss(
+            self, p_aux, p_cls, t_cls, p_exp, t_exp, mask, weights, epoch=None
+    ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
         optional = {}
         selection = weights['selection']
         lasso = weights['lasso']

latent_rationale/mtl_e2e/utils.py
@@ -50,9 +50,8 @@ class PaddedSequence:
             batch_lengths = torch.LongTensor([len(x) for x in data])
         return PaddedSequence(padded, batch_lengths, batch_first).to(device=device)
 
-    #@classmethod
-    #def autopad(cls, data, len_queries, max_length, batch_first, device):
-
+    # @classmethod
+    # def autopad(cls, data, len_queries, max_length, batch_first, device):
 
     def pack_other(self, data: torch.Tensor):
         return pack_padded_sequence(data, self.batch_sizes, batch_first=self.batch_first, enforce_sorted=False)
@@ -92,7 +91,7 @@ class PaddedSequence:
     def unpad(self, other: torch.Tensor) -> List[torch.Tensor]:
         out = []
         for o, bl in zip(other, self.batch_sizes):
-            out.append(torch.cat((o[:bl], torch.zeros(max(0, bl-len(o))))))
+            out.append(torch.cat((o[:bl], torch.zeros(max(0, bl - len(o))))))
         return out
 
     def flip(self) -> 'PaddedSequence':
@@ -126,37 +125,37 @@ def get_args():
     args = vars(args)
     conf_fname = args['conf_fname']
     with open(conf_fname, 'r') as fin:
-        conf = json.load(fin)
+        training_conf = json.load(fin)
 
-    for k, v in args.items(): # values in args overwrites the ones on conf file
+    for k, v in args.items():  # values in args overwrites the ones on conf file
         if v is None:
             continue
         if k in ('selection', 'w_aux', 'w_exp'):
-            conf['weights'][k] = v
+            training_conf['weights'][k] = v
         else:
-            conf[k] = v
-    conf["eval_batch_size"] = max(conf['batch_size'], conf['eval_batch_size'])
+            training_conf[k] = v
+    training_conf["eval_batch_size"] = max(training_conf['batch_size'], training_conf['eval_batch_size'])
 
-    conf['model_common']['num_labels'] = len(conf['classes'])
+    training_conf['model_common']['num_labels'] = len(training_conf['classes'])
 
-    mtl_conf = copy.deepcopy(conf['model_common'])
-    if 'mtl' in conf:
-        mtl_conf.update(conf['mtl'])
-    conf['mtl'] = mtl_conf
+    mtl_conf = copy.deepcopy(training_conf['model_common'])
+    if 'mtl' in training_conf:
+        mtl_conf.update(training_conf['mtl'])
+    training_conf['mtl'] = mtl_conf
 
-    cls_conf = copy.deepcopy(conf['model_common'])
-    if 'cls' in conf:
-        cls_conf.update(conf['cls'])
-    conf['cls'] = cls_conf
+    cls_conf = copy.deepcopy(training_conf['model_common'])
+    if 'cls' in training_conf:
+        cls_conf.update(training_conf['cls'])
+    training_conf['cls'] = cls_conf
 
-    model_conf = E2ExPredConfig(conf)
-    return conf, model_conf
+    model_conf = E2ExPredConfig(training_conf)
+    return training_conf, model_conf
 
 
-def prepare_minibatch(mb: List[Example],
-                      tokenizer: BertTokenizer,
-                      max_length: int=512,
-                      device: str=None):
+def bert_input_preprocess(examples: List[Example],
+                          tokenizer: BertTokenizer,
+                          max_length: int = 512,
+                          device: str = None):
     """
     Minibatch is a list of examples.
     This function converts words to IDs and returns
@@ -165,25 +164,20 @@ def prepare_minibatch(mb: List[Example],
     cls_token_id = tokenizer.cls_token_id
     sep_token_id = tokenizer.sep_token_id
     pad_token_id = tokenizer.pad_token_id
-    cls_token = torch.tensor([cls_token_id])#.to(device=device)
-    sep_token = torch.tensor([sep_token_id])#.to(device=device)
+    cls_token = torch.tensor([cls_token_id])  # .to(device=device)
+    sep_token = torch.tensor([sep_token_id])  # .to(device=device)
     inputs = []
     exps = []
     labels = []
     position_ids = []
-    for inst in mb:
+    for inst in examples:
         q = inst.query
         d = inst.tokens
         exp = inst.token_labels
         labels.append(inst.label)
         if len(q) + len(d) + 2 > max_length:
-            # d = torch.Tensor(d[:(max_length - len(q) - 2)]).type_as(cls_token)
-            # exp = torch.Tensor(exp[:(max_length - len(q) - 2)])
             d = d[:(max_length - len(q) - 2)]
             exp = exp[:(max_length - len(q) - 2)]
-        # q = torch.Tensor(q).type_as(cls_token)
-        # print(cls_token.__class__, q.__class__, exp.__class__)
-        # print(cls_token.type(), q.type(), exp.type())
         inputs.append(torch.cat([cls_token, q, sep_token, d]))
         exps.append(torch.cat([torch.Tensor([0] * (len(q) + 2)), exp]))
         position_ids.append(torch.tensor(list(range(0, len(q) + 1)) + list(range(0, len(d) + 1))))  # tokens
@@ -192,56 +186,30 @@ def prepare_minibatch(mb: List[Example],
     inputs = PaddedSequence.autopad(inputs, batch_first=True, padding_value=pad_token_id, device=device)
     positions = PaddedSequence.autopad(position_ids, batch_first=True, padding_value=0, device=device)
     exps = PaddedSequence.autopad(exps, batch_first=True, padding_value=0, device=device)
-    attention_masks = inputs.mask(on=1., off=-1000.).type(torch.float).to(device=device)
-    padding_masks = inputs.mask(on=1., off=0.).type(torch.bool).to(device=device)
+    attention_masks = inputs.mask(on=1, off=0).type(torch.float).to(device=device)
+    padding_masks = inputs.mask(on=1, off=0).type(torch.bool).to(device=device)
     labels = torch.LongTensor(labels).to(device=device)
     return inputs, exps, labels, positions, attention_masks, padding_masks
 
 
-    #
-    #
-    # queries, documents, exps, labels = [], [], [], []
-    # for inst in mb:
-    #     q = inst.query
-    #     queries.append(torch.Tensor(q))
-    #     documents.append(torch.Tensor(inst.tokens))
-    #     exps.append(torch.cat(torch.Tensor([0] * (len(q) + 2))inst.token_labels))
-    #     labels.append(torch.Tensor([inst.label]))
-    #
-    # return queries, documents, exps, labels
+# def numerify_labels(dataset, labels_mapping):
+#     for exp_id, exp in enumerate(dataset):
+#         dataset[exp_id] = Example(tokens=exp.tokens,
+#                                   label=labels_mapping[exp.label],
+#                                   token_labels=exp.token_labels,
+#                                   query=exp.query,
+#                                   ann_id=exp.ann_id,
+#                                   docid=exp.docid)
+#     return dataset
 
 
-# def prepare_for_input(example: Example,
-#                       max_length: int,
-#                       tokenizer: BertTokenizer,
-#                       device: str = 'cpu'):
-#     q = example.query
-#     d = example.tokens
-#     cls_token = [tokenizer.cls_token_id]
-#     sep_token = [tokenizer.sep_token_id]
-#     pad_token_id = tokenizer.pad_token_id
-#     if len(q) + len(d) + 2 > max_length:
-#         d = d[:(max_length - len(q) - 2)]
-#     input = torch.cat([cls_token, q, sep_token, d])
-#     selector_mask_starts = torch.Tensor([len(q) + 2]).to(device)
-#     input = PaddedSequence.autopad(input, batch_first=True, padding_value=pad_token_id,
-#                                    device=device)
-#     attention_mask = input.mask(on=1., off=0., device=device)
-#     attributes = [0] * (len(example.query) + 2) + example.tokens
-#     return [input, attributes, example.label]
-
-
-def numerify_labels(dataset, labels_mapping):
-    for exp_id, exp in enumerate(dataset):
-        # print(exp_id)
-        # print(dataset[exp_id])
-        # print(exp)
-        dataset[exp_id] = Example(tokens=exp.tokens,
-                                  label=labels_mapping[exp.label],
-                                  token_labels=exp.token_labels,
-                                  query=exp.query,
-                                  ann_id=exp.ann_id,
-                                  docid=exp.docid)
+def numerify_label(ann, labels_mapping):
+    return Example(tokens=ann.tokens,
+                   label=labels_mapping[ann.label],
+                   token_labels=ann.token_labels,
+                   query=ann.query,
+                   ann_id=ann.ann_id,
+                   docid=ann.docid)
     return dataset
 
 
@@ -260,8 +228,8 @@ def tokenize_query_doc(example: Example, tokenizer: Any):
         tokens.extend(token_pieces)
         token_labels.extend([token_label] * len(token_pieces))
     if isinstance(tokenizer, BertTokenizer):
-        return Example(query=torch.LongTensor(query_tokens),#.type(torch.long),
-                       tokens=torch.LongTensor(tokens),#.type(torch.long),
+        return Example(query=torch.LongTensor(query_tokens),  # .type(torch.long),
+                       tokens=torch.LongTensor(tokens),  # .type(torch.long),
                        token_labels=torch.Tensor(token_labels),
                        label=torch.Tensor([example.label]),
                        ann_id=example.ann_id,